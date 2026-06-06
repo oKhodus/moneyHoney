b9d3b74b5323ffef57d21475d9340f1c9f6d6edc
Oleksii Khodus <exalchel@gmail.com>
2026-06-06 23:21:50 +0300

    feat: add income() function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+double balance = 0;
+
+void income(double value){
+    if (value > 0){
+        balance += value;
+        std::cout << "Congrats' you have added income :)" << std::endl;
+    } else {
+        std::cout << "Error: Income's value must to be more than 0" << std::endl;
+    }
+}
+
 int main(){
     double UserValue;
-    std::cout << "Enter a value: ";
+    std::cout << "Enter your new income: ";
     std::cin >> UserValue;
-    std::cout << "Users's value: " << UserValue;
+    income(UserValue);
+    std::cout << "Your current balance is: " << balance << std::endl;
     return 0;
 }
