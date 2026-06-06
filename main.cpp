ab214fa829950dcbea7c16347ca7960c0803e52b
Oleksii Khodus <exalchel@gmail.com>
2026-06-06 22:58:24 +0300

    Init proj

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    double UserValue;
+    std::cout << "Enter a value: ";
+    std::cin >> UserValue;
+    std::cout << "Users's value: " << UserValue;
+    return 0;
+}
