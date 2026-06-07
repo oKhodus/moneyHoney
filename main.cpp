86e3a4bcca6e9bfd3dd353f9504ff788fa0a0d15
Oleksii Khodus <exalchel@gmail.com>
2026-06-07 23:35:48 +0300

    feat: add expence() function

--- main.cpp
+++ main.cpp
@@ -11,11 +11,24 @@ void income(double value){
     }
 }
 
+void expense(double value) {
+    if (value > balance){
+        std::cout << "Error: Expense's value must to be more than 0" << std::endl;
+    }else {
+        balance -= value;
+        std::cout << "Congrats' you have decreased your balance :)" << std::endl;
+    }
+}
+
 int main(){
     double UserValue;
     std::cout << "Enter your new income: ";
     std::cin >> UserValue;
     income(UserValue);
+    std::cout << "Your current balance is: " << balance << std::endl;
+    std::cout << "Enter your new expense: ";
+    std::cin >> UserValue;
+    expense(UserValue);
     std::cout << "Your current balance is: " << balance << std::endl;
     return 0;
 }
