cef05a74b7a5d6ffb7ddf29ee0519c81cf2a98f9
Oleksii Khodus <exalchel@gmail.com>
2026-06-10 18:57:14 +0300

    refactor: extract cmds & cmdlayer

---
+++ cmdlayer.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include "cmds.cpp"
+using namespace std;
+using namespace CMDS;
+
+
+namespace CMDLayer {
+    void CmdLayer(string cmd) {
+
+        if (cmd == "/add")
+        {
+            double UserValue;
+            cout << "Enter your new expense: ";
+            cin >> UserValue;
+            income(UserValue);
+            showBalance();
+        }
+        else if (cmd == "/bal")
+        {
+            showBalance();
+        }
+        else if (cmd == "/exp")
+        {
+            double UserValue;
+            cout << "Enter your new income: ";
+            cin >> UserValue;
+            expense(UserValue);
+            showBalance();
+        }
+        else {
+            cout << "Error: Incorrect command :(";
+        }
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,60 +1,8 @@
 #include <iostream>
 #include <string>
+#include "cmdlayer.cpp"
 using namespace std;
-
-double balance = 0;
-
-void income(double value){
-    
-    (value > 0) ? balance += value : balance;
-    
-    string result = (value > 0) ? 
-    "Congrats' you have added income :)" : 
-    "Error: Income's value must to be more than 0";
-    
-    cout << result << endl;
-}
-
-void expense(double value) {
-    (value > balance) ? balance : balance -= value;
-
-    string result = (value > 0) ? 
-    "Congrats' you have decreased your balance :)" : 
-    "Error: Expense's value must to be more than 0";
-    
-    cout << result << endl;
-}
-
-void showBalance() {
-    cout << "Your current balance is: " << balance << endl;
-}
-
-void CmdLayer(string cmd) {
-
-    if (cmd == "/add")
-    {
-        double UserValue;
-        cout << "Enter your new expense: ";
-        cin >> UserValue;
-        income(UserValue);
-        showBalance();
-    }
-    else if (cmd == "/bal")
-    {
-        showBalance();
-    }
-    else if (cmd == "/exp")
-    {
-        double UserValue;
-        cout << "Enter your new income: ";
-        cin >> UserValue;
-        expense(UserValue);
-        showBalance();
-    }
-    else {
-        cout << "Error: Incorrect command :(";
-    }
-}
+using namespace CMDLayer;
 
 int main(){
     cout << "Welcome to the moneyHoney app! ^_^ " << endl;
